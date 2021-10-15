3051d41adb824096180bd62fdbbf4ad336112d80
Neiran <pepega@gmail.com>
2021-10-15 20:11:05 +0300

    Created ListView with ArrayAdapter

--- app/src/main/java/com/example/shpargalkafish/MainActivity.java
+++ app/src/main/java/com/example/shpargalkafish/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.shpargalkafish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] fishes = {"Shuka", "Sudak", "Okun", "Ersh", "Plotva", "Redperka", "Lesh", "Sazan", "Karp"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView fishList = (ListView) findViewById(R.id.fishList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fishes);
     }
+
+
 }
