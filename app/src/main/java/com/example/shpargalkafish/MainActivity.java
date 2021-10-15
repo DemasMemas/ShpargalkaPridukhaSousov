21b75b25a898d61bc5482c58b6df891cf63ed35c
Neiran <pepega@gmail.com>
2021-10-15 20:21:21 +0300

    Added Array and onItemClickListener

--- app/src/main/java/com/example/shpargalkafish/MainActivity.java
+++ app/src/main/java/com/example/shpargalkafish/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.shpargalkafish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView selection = (TextView) findViewById(R.id.selection);
         ListView fishList = (ListView) findViewById(R.id.fishList);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fishes);
+        fishList.setAdapter(adapter);
+        fishList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
+                String selectedItem = fishes[pos];
+                selection.setText(selectedItem);
+            }
+        });}
     }
-
-
-}
